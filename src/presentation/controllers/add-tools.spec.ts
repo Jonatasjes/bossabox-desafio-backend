6869f2fd71ff12409343c9f80c41fa77bf3f2280
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-06-21 22:09:29 -0300

    test: add tools controller returns 500 if AddToolsController throws

--- src/presentation/controllers/add-tools.spec.ts
+++ src/presentation/controllers/add-tools.spec.ts
@@ -1,15 +1,39 @@
+import { ToolModel } from '../../domain/models/tool'
+import { AddTool, AddToolModel } from '../../domain/usecases/add-tools'
 import { InvalidTagsParamError } from '../errors/invalid-tags-params-error'
 import { MissingParamsError } from '../errors/missing-params-error'
+import { ServerError } from '../errors/server-error'
 import { AddToolsController } from './add-tools'
+
+const makeTools = (): AddTool => {
+  class AddToolsStub implements AddTool {
+    async add(tool: AddToolModel): Promise<ToolModel> {
+      const fakeTool = {
+        id: 'valid_id',
+        title: 'valid_title',
+        link: 'valid_link',
+        description: 'valid_description',
+        tags: ['valid_tags']
+      }
+
+      return await new Promise(resolve => resolve(fakeTool))
+    }
+  }
+
+  return new AddToolsStub()
+}
 
 interface SutTypes {
   sut: AddToolsController
+  addToolsStub: AddTool
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new AddToolsController()
+  const addToolsStub = makeTools()
+  const sut = new AddToolsController(addToolsStub)
   return {
-    sut
+    sut,
+    addToolsStub
   }
 }
 
@@ -83,5 +107,24 @@ describe('Add tools controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidTagsParamError())
+  })
+
+  test('Should return 500 if AddToolsController throws', async () => {
+    const { sut, addToolsStub } = makeSut()
+    jest.spyOn(addToolsStub, 'add').mockImplementationOnce(async () => {
+      return await new Promise((resolve, reject) => reject(new Error()))
+    })
+    const httpRequest = {
+      body: {
+        title: 'any_title',
+        link: 'any_link',
+        description: 'any_description',
+        tags: ['any_tags']
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    console.log(httpResponse)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
