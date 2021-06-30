74e614d9472ec1082e0874f69c104bbb153627e2
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-06-30 19:50:28 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/tool-routes.test.ts
+++ src/main/routes/tool-routes.test.ts
@@ -1,7 +1,20 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('Tools Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const toolCollection = MongoHelper.getCollection('tools')
+    await toolCollection.deleteMany({})
+  })
+
   test('Shold return an account on success', async () => {
     await request(app)
       .post('/api/v0/tools')
